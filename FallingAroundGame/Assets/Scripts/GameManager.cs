739af6f353ffbce62d46c907b9a380d25b4ddb42
aneudissalcedo <aneudissalcedo@gmail.com>
2018-08-13 02:00:05 -0700

    Added environment tiles through GameManager.cs script and made prefabs of the game objects acquired.

--- FallingAroundGame/Assets/Scripts/GameManager.cs
+++ FallingAroundGame/Assets/Scripts/GameManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject normalFloor;
-    [SerializeField] private GameObject lavaFloor;
     [SerializeField] private int columns = 20;
     [SerializeField] private int rows = 20;
+
+    [Header("FloorSettings")]
+    [SerializeField] private GameObject normalFloor;
+    [SerializeField] private GameObject lavaFloor;
+
+    [Space]
+    [Header("RockSettings")]
+    [SerializeField] private GameObject[] rocks;
+
     private Transform floorHolder;
     private Transform lavaHolder;
 
@@ -25,11 +32,17 @@ public class GameManager : MonoBehaviour
     private void FloorSetup()
     {
         floorHolder = new GameObject("Floors").transform;
-        for(int x=0; x<columns; x++)
+        for(int x=-1; x<columns +1; x++)
         {
-            for(int y=0; y<rows; y++)
+            for(int y=-1; y<rows +1; y++)
             {
-                GameObject instance = Instantiate(normalFloor, new Vector3(x, 0f, y), Quaternion.identity) as GameObject;
+                GameObject toInstantiate = normalFloor;
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = rocks[Random.Range(0, rocks.Length)];
+                }
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, 0f, y), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(floorHolder);
             }
         }
@@ -37,9 +50,9 @@ public class GameManager : MonoBehaviour
     private void LavaSetup()
     {
         lavaHolder = new GameObject("Lavas").transform;
-        for (int x = 0; x < columns; x++)
+        for (int x = -1; x < columns +1; x++)
         {
-            for (int y = 0; y < rows; y++)
+            for (int y = -1; y < rows +1; y++)
             {
                 GameObject instance = Instantiate(lavaFloor, new Vector3(x, -0.5f, y), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(lavaHolder);
